26cc5cdbbdf7d1a642a5a3026294899908a92783
Alina Pazderska <alina.pazderska@gmail.com>
2018-05-12 17:41:28 +0200

    one more file

---
+++ PointController.java
@@ -0,0 +1,27 @@
+package PointController;
+
+public class PointController {
+    public static void addX() {
+        int x = 5;
+        x++;
+        System.out.print(x + ", "); // increase variable x with value of 1
+    }
+
+    public static void addY() {
+        int y = 10;
+        y++;
+        System.out.println(y); // increase variable y with value of 1
+    }
+
+    public static void minusX() {
+        int x = 5;
+        x--;
+        System.out.print(x + ", "); // decrease variable x with value of 1
+    }
+
+    public static void minusY() {
+        int y = 10;
+        y--;
+        System.out.println(y); // decrease variable y with value of 1
+    }
+}
